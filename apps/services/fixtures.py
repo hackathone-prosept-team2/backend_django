13e68be7bd3aa8db1bc1952f08603990f1a2fa3f
Ruslan Atarov <ruslanatarov@gmail.com>
2023-11-28 16:59:59 +0300

    tmp solution for prices upload

--- apps/services/fixtures.py
+++ apps/services/fixtures.py
@@ -1,7 +1,9 @@
 import csv
 from pathlib import Path
 
+from apps.dealers.crud import get_keys_values, bulk_create_keys
 from apps.dealers.models import Dealer, DealerKey
+from apps.prices.models import DealerPrice
 from apps.products.models import Product
 
 
@@ -42,3 +44,36 @@ def get_dealers_keys_datasets() -> list[DealerKey]:
     with open(filename, mode="r", encoding="utf8") as file:
         reader = csv.DictReader(file, delimiter=";")
         return [DealerKey(is_provided=True, **row) for row in reader]
+
+
+def get_prices_datasets() -> list[DealerPrice]:
+    filename = Path("static", "fixtures", "marketing_dealerprice_copy.csv")
+    with open(filename, mode="r", encoding="utf8") as file:
+        reader = csv.DictReader(file, delimiter=";")
+        keys_in_db_set = get_keys_values()
+        new_keys = []
+        for row in reader:
+            dealer_key = (
+                row["product_key"]
+                if row["product_key"]
+                else row["product_url"]
+            )
+            if dealer_key not in keys_in_db_set:
+                new_keys.append(
+                    DealerKey(key=dealer_key, dealer=row["dealer_id"])
+                )
+        bulk_create_keys(new_keys)
+        keys_in_db_set = get_keys_values()
+        new_prices = []
+        for row in reader:
+            fields = {
+                "id": row["id"],
+                "key_id": keys_in_db_set[row["product_key"]],
+                "price": row["price"],
+                "product_url": row["product_url"],
+                "name": row["product_name"],
+                "date": row["date"],
+            }
+            new_prices.append(DealerPrice(**fields))
+        print(new_prices)
+    return new_prices
